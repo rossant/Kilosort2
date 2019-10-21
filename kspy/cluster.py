090b3acff6c4a8bc93a5ce9382e00bac53a85bd9
Cyrille Rossant <rossant@users.noreply.github.com>
2019-10-21 16:39:54 +0200

    Add sortBatches2

--- kspy/cluster.py
+++ kspy/cluster.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cupy as cp
 from preprocess import my_min, my_sum
+from cptools import svdecon
+from utils import p
 
 
 def get_SpikeSample(dataRAW, row, col, params):
@@ -78,3 +80,58 @@ def isolated_peaks_new(S1, params):
     mu = -peaks[row, col]
 
     return row, col, mu
+
+
+def sortBatches2(ccb0):
+    # takes as input a matrix of nBatches by nBatches containing
+    # dissimilarities.
+    # outputs a matrix of sorted batches, and the sorting order, such that
+    # ccb1 = ccb0(isort, isort)
+
+    # put this matrix on the GPU
+    ccb0 = cp.asarray(ccb0)
+
+    # compute its svd on the GPU (this might also be fast enough on CPU)
+    u, s, v = svdecon(ccb0)
+
+    # initialize the positions xs of the batch embeddings to be very small but proportional to the first PC
+    xs = .01 * u[:, 0] / cp.std(u[:, 0], ddof=1)
+
+    # 200 iterations of gradient descent should be enough
+    niB = 200
+
+    # this learning rate should usually work fine, since it scales with the average gradient
+    # and ccb0 is z-scored
+    eta = 1
+    for k in range(niB):
+        # euclidian distances between 1D embedding positions
+        ds = (xs - xs[:, np.newaxis]) ** 2
+        # the transformed distances go through this function
+        W = cp.log(1 + ds)
+
+        # the error is the difference between ccb0 and W
+        err = ccb0 - W
+
+        # ignore the mean value of ccb0
+        err = err - cp.mean(err)
+
+        # backpropagate the gradients
+        err = err / (1 + ds)
+        err2 = err * (xs[:, np.newaxis] - xs)
+        D = cp.mean(err2, axis=1)  # one half of the gradients is along this direction
+        E = cp.mean(err2, axis=0)  # the other half is along this direction
+        # we don't need to worry about the gradients for the diagonal because those are 0
+
+        # final gradients for the embedding variable
+        dx = -D + E.T
+
+        # take a gradient step
+        xs = xs - eta * dx
+
+    # sort the embedding positions xs
+    isort = cp.argsort(xs, axis=0)
+
+    # sort the matrix of dissimilarities
+    ccb1 = ccb0[isort, :][:, isort]
+
+    return ccb1, isort
